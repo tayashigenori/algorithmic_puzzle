d8c454800c680644ae29537eea933a9f594d6be4
Shigenori Taya <shigenori.taya@gmail.com>
2014-06-05 10:25:19 +0900

    some refactor in 026 "Find the Rank" (stopping using list.remove)

--- 026_Find_the_Rank.py
+++ 026_Find_the_Rank.py
@@ -3,19 +3,32 @@
 import math
 import sys
 
+CHAR_USED = True
+CHAR_UNUSED = False
+
 class String(str):
     def __init__(self, s = "", alphabet_dict = {}):
         self._s = s
         self._alphabets = self.get_alphabets()
         return
 
+    """
     def get_alphabets(self,):
         alphabets = [c for c in self._s]
         return sorted(alphabets)
+    """
+    def get_alphabets(self,):
+        return dict([(c, CHAR_UNUSED) for c in self._s])
+    def get_unused_chars(self,):
+        return [c for c,status in self._alphabets.items()
+                if status == CHAR_UNUSED]
+    def set_alphabet_status(self, c, status):
+        self._alphabets[c] = CHAR_USED
+        return
 
     def find_the_rank(self,):
         ######################################
-        # for TURING, count the followings:
+        # for "TURING", count the followings:
         #  - G***** ... 5!
         #  - I***** ... 5!
         #  - N***** ... 5!
@@ -39,13 +52,18 @@ class String(str):
         for i in range(len(self._s)): # i: 0 ~ 5
             i_mojime = i+1
             c = self._s[i]
-            cs_rank_in_alphabet = self._alphabets.index(c)
+            # この c がいま未使用なアルファベットの中で何番目か (rank) をチェックする
+            #cs_rank_in_alphabet = self._alphabets.index(c)
+            unused_chars = self.get_unused_chars()
+            cs_rank_in_alphabet = sorted(unused_chars).index(c)
             sys.stderr.write("### adding math.factorial(%d) * %d\n"
                              %(keta - i_mojime, cs_rank_in_alphabet ))
+            # rank を計算
             rank += math.factorial(keta - i_mojime) * cs_rank_in_alphabet
-            # alphabet から使用済みの文字 c を削除
-            self._alphabets.remove(c)
-        # self._s の前に rank 個の要素がある
+            # c を使用済みとする
+            #self._alphabets.remove(c)
+            self.set_alphabet_status(c, CHAR_UNUSED)
+        # self._s の前に rank 個の要素があるということなので 1 を加算
         return rank + 1
 
 def main():
